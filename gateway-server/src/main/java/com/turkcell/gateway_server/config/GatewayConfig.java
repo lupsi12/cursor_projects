7120457d6e5208f7e93c251abce7041c863804d6
isilkefal <kefalisil9@gmail.com>
2025-10-19 15:33:06 +0300

    GatewayConfig file edited for product-service

--- gateway-server/src/main/java/com/turkcell/gateway_server/config/GatewayConfig.java
+++ gateway-server/src/main/java/com/turkcell/gateway_server/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.turkcell.gateway_server.config;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,9 +14,14 @@ public class GatewayConfig {
                 .routes()
                 .route("product-service", r -> r
                         .path("/api/v1/products/**")
-                        .filters(f -> f.retry(config -> config.setRetries(3))
-                        )
+                        .filters(f -> f
+                                .retry(config -> config.setRetries(3)))
                         .uri("lb://product-service"))
+
+                .route("fallback", r -> r
+                        .path("/**")
+                        .filters(f -> f.setStatus(HttpStatus.NOT_FOUND))
+                        .uri("no://op"))
                 .build();
     }
 
